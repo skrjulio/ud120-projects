e1ef811872dc35d124868b83c569976aefb65d59
S. Julio <shelley.julio@gmail.com>
2016-02-03 16:34:42 -0800

    dt_author_id.py complete

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,6 +12,9 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+import numpy as np
 
 
 ### features_train and features_test are the features for the training
@@ -23,8 +26,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+t0 = time()
+clf = clf.fit(features_train, labels_train)
+print "Training time:", round(time()-t0, 3)
+t0 = time()
+pred = clf.predict(features_test)
+print "Predicting time:", round(time()-t0,3)
+acc = accuracy_score(labels_test, pred)
+print(acc)
+print np.size(features_train,1)
 ### your code goes here ###
-
 
 #########################################################
 
